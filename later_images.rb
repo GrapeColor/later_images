7d239e3561f59f6048b3a5c1c87c2440ad988496
GrapeColor <grapecolor@users.noreply.github.com>
2019-10-23 03:58:40 +0900

    Corresponding to NSFW, and optimize code

--- later_images.rb
+++ later_images.rb
@@ -6,40 +6,34 @@ Dotenv.load
 
 bot = Discordrb::Bot.new(
   client_id: ENV['DISCORD_CLIENT_ID'],
-  token: ENV['DISCORD_TOKEN']
+  token:     ENV['DISCORD_TOKEN']
 )
 
-@client = Twitter::REST::Client.new do |config|
+client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
   config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
   config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
-bot.ready do
-  bot.game = 'Twitter'
-end
+bot.ready { bot.game = "Twitter" }
 
-bot.message do |event|
-  content = event.message.content
-  m = content.match(/https:\/\/twitter.com\/([a-zA-Z0-9_]+)\/status\/([0-9]+)/)
-  next if m.nil?
+bot.message(attributes = {contains: "https://twitter.com/"}) do |event|
+  match_url = event.content.match(%r{https://twitter.com/([a-zA-Z0-9_]+)/status/([0-9]+)})
+  next if match_url.nil?
 
-  twitter_url = m[0]
-  tweet = @client.status(m[2])
+  tweet = client.status(match_url[2])
+  next if tweet.attrs[:possibly_sensitive] && !event.channel.nsfw
 
   if tweet.media?
     media = tweet.media
-    photo_urls = []
     if media[0].type == "photo"
-      media.each { |m| photo_urls.push(m.media_url_https.to_s) }
+      photo_urls = media.map { |m| m.media_url_https.to_s }
     end
 
     if photo_urls.length > 1
       photo_urls.shift
-      photo_urls.each do |url|
-        event.respond url
-      end
+      photo_urls.each { |url| event << url }
     end
   end
 end
