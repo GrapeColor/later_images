c4604db1e356eb8390aa05e06af244b30efba103
GrapeColor <grapecolor@users.noreply.github.com>
2019-10-24 01:02:43 +0900

    Fixed an issue that did not respond to Embed

--- later_images.rb
+++ later_images.rb
@@ -21,9 +21,6 @@ bot.ready { bot.game = "Twitter" }
 
 # "https://twitter.com/"を含むメッセージ
 bot.message(attributes = {contains: "https://twitter.com/"}) do |event|
-  # Embedがあるか
-  next if event.message.embeds.empty?
-
   # URLがマッチするか
   match_url = event.content.match(%r{https://twitter.com/(\w+)/status/(\d+)})
   next if match_url.nil?
@@ -31,11 +28,15 @@ bot.message(attributes = {contains: "https://twitter.com/"}) do |event|
   # TweetはNSFWではないか
   tweet = client.status(match_url[2])
   next if tweet.attrs[:possibly_sensitive] && event.channel.nsfw == false
+  
+  # Embedがあるか(Embedが送られるまで待つ)
+  sleep(0.5)
+  next if event.channel.load_message(event.message.id).embeds.empty?
 
   # 画像URLを取得
   tweet.media.each_with_index do |m, index|
     next if index < 1 || m.type != "photo"
-    event << "<REPLY TO: " + event.message.id.to_s(36) + ">" if index == 1
+    event << "\u27A1 " + event.message.id.to_s(36) if index == 1
     event << m.media_url_https.to_s
   end
 end
@@ -48,7 +49,8 @@ bot.message_delete do |event|
     next if message.author != bot.profile.id
     
     # リプライ先メッセージIDはあるか
-    match_reply = message.content.match(%r{<REPLY TO: ([a-z0-9]+)>})
+    match_reply = message.content.match(/[\u27A1] ([a-z0-9]+)/) # New pattern
+    match_reply = message.content.match(%r{<REPLY TO: ([a-z0-9]+)>}) if match_reply.nil?
     next if match_reply.nil?
 
     # 削除メッセージIDと一致するか
