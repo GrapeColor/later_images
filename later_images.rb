749c9b00600f863d0d5dd47b16e773adff15f38a
GrapeColor <grapecolor@users.noreply.github.com>
2019-10-31 08:22:26 +0900

    Class methods and constants

--- later_images.rb
+++ later_images.rb
@@ -1,26 +1,13 @@
-require "bundler/setup"
+require 'bundler/setup'
 require 'dotenv'
 require 'discordrb'
-require 'twitter'
 
 Dotenv.load
-EMBED_TIMEOUT = 30  # Embed埋め込み待機時間
-DELETE_RANGE  = 10  # 削除メッセージ検索範囲
-TEMP_SECOND   = 10  # 一時メッセージ表示時間
+require './message.rb'
 
 pending_messages = {} # Embed埋め込み待ちメッセージ
 
-bot = Discordrb::Bot.new(
-  client_id: ENV['DISCORD_CLIENT_ID'],
-  token:     ENV['DISCORD_TOKEN']
-)
-
-@client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-end
+bot = Discordrb::Bot.new(client_id: ENV['DISCORD_CLIENT_ID'], token: ENV['DISCORD_TOKEN'])
 
 # ステータス表示を設定
 bot.ready { bot.game = "Twitter | @" + bot.profile.distinct }
@@ -29,62 +16,33 @@ bot.ready { bot.game = "Twitter | @" + bot.profile.distinct }
 bot.heartbeat do
   # タイムアウトしたメッセージを破棄
   now = Time.now
-  pending_messages.delete_if { |id, message| now - message.timestamp > EMBED_TIMEOUT }
+  pending_messages.delete_if { |id, message| now - message.timestamp > Message::EMBED_TIMEOUT }
 end
 
-# メッセージ生成
-def message_generater(event, message_id, content)
-  # URLがマッチするか
-  match_url = content.match(%r{!?https?://twitter.com/\w+/status/(\d+)})
-  return if match_url.nil? || match_url[0].start_with?("!")
-  tweet = @client.status(match_url[1], { tweet_mode: "extended" })
-  
-  # 画像が2枚以上あるか
-  media = tweet.media.dup
-  return if media.length <= 1 || media[0].type != "photo"
-  media.shift
-  
-  # ツイートはNSFWではないか
-  if tweet.attrs[:possibly_sensitive] && !event.channel.nsfw?
-    event.send_temporary_message("**ツイートにセンシティブな内容が含まれる可能性があるため、画像を表示できません**", TEMP_SECOND)
-    return
-  end
-  
-  # メッセージID・画像URL挿入
-  event << "メッセージ(ID: #{message_id})のツイート画像"
-  media.each { |m| event << m.media_url_https.to_s }
-
-  # 削除メッセージの検索範囲外ではないか
-  if event.channel.history(DELETE_RANGE, nil, message_id).length >= DELETE_RANGE
-    event.send_temporary_message("BOTが応答するまでの間にチャンネルに既定以上のメッセージが送信されました", TEMP_SECOND)
-    event.drain
-  end
-end
-
-# ツイートURLを含むメッセージ
+# ツイートURLを含むメッセージの送信
 bot.message({ contains: "://twitter.com/" }) do |event|
   if event.message.embeds.empty?
     pending_messages[event.message.id] = event.message
     next
   else
-    message_generater(event, event.message.id, event.content)
+    Message.generater(event, event.message.id, event.content)
   end
 end
 
 # メッセージの更新
 bot.message_update do |event|
   if (message = pending_messages[event.message.id]) && !event.message.embeds.empty?
-    message_generater(event, event.message.id, message.content)
+    Message.generater(event, event.message.id, message.content)
   end
 end
 
 # メッセージの削除
 bot.message_delete do |event|
   # 削除メッセージ以降のメッセージを検索
-  event.channel.history(DELETE_RANGE, nil, event.id).each do |message|
+  event.channel.history(Message::DELETE_RANGE, nil, event.id).each do |message|
     # bot自身のメッセージか
     next if message.author != bot.profile.id
-    
+
     # メッセージIDはあるか
     match_reply = message.content.match(/(ID:|[\u{1f194}]|[\u27A1]|REPLY TO:) ([a-z0-9]+)/)
     next if match_reply.nil?
---
+++ message.rb
@@ -0,0 +1,48 @@
+class Message
+  require 'twitter'
+
+  # 定数初期化
+  EMBED_TIMEOUT = 30  # Embed埋め込み待機時間
+  DELETE_RANGE  = 10  # 削除メッセージ検索範囲
+  TEMP_SECOND   = 20  # 一時メッセージ表示時間
+
+  # Twitter APIクライアント初期化
+  @@client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  end
+
+  # メッセージ生成
+  ## event      = Discordrb::Events
+  ## message_id = Discordrb::Message.id
+  ## content    = Discordrb::Message.content
+  def self.generater(event, message_id, content)
+    # URLがマッチするか
+    match_url = content.match(%r{!?https?://twitter.com/\w+/status/(\d+)})
+    return if match_url.nil? || match_url[0].start_with?("!")
+    tweet = @@client.status(match_url[1], { tweet_mode: "extended" })
+
+    # 画像が2枚以上あるか
+    media = tweet.media.dup
+    return if media.length <= 1 || media[0].type != "photo"
+    media.shift
+
+    # ツイートはNSFWではないか
+    if tweet.attrs[:possibly_sensitive] && !event.channel.nsfw?
+      event.send_temporary_message("**ツイートにセンシティブな内容が含まれる可能性があるため、画像を表示できません（NSFWチャンネルでのみ表示可）**", TEMP_SECOND)
+      return
+    end
+
+    # メッセージID・画像URL挿入
+    event << "メッセージ(ID: #{message_id})のツイート画像"
+    media.each { |m| event << m.media_url_https.to_s }
+
+    # 削除メッセージの検索範囲外ではないか
+    if event.channel.history(DELETE_RANGE, nil, message_id).length >= DELETE_RANGE
+      event.send_temporary_message("BOTが応答するまでの間にチャンネルに既定以上のメッセージが送信されました", TEMP_SECOND)
+      event.drain
+    end
+  end
+end
